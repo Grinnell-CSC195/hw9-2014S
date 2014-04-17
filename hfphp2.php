068c5ee45d147e6be33874695682a9d32f22f3c2
Halley Freger <fregerha17@cs.grinnell.edu>
2014-04-16 20:16:40 -0500

    added halley's php

---
+++ hfphp2.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+
+<title>color</title>
+
+</head>
+
+<?php
+
+$color = $_REQUEST['color'] ;
+
+?>
+
+<body bgcolor="#F88017" text="#000000">
+
+
+<p>
+
+You said <?php print $color; ?>
+
+<p>The correct answer is orange!
+
+</body>
+
+</html>
--- links.md
+++ links.md
@@ -1,1 +1,2 @@
 mitchell17: https://dormsdb.alexthemitchell.com
+fregerha17: http://www.cs.grinnell.edu/~fregerha17/hfphp.html
